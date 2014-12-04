a0feb3da7ac151fa4333d662d57c3a6d9bc8858f
Bing-Qian Luan <bingqian.luan@teleyi.com>
2014-12-05 00:49:26 +0800

    solve 1d

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
-from flask import Flask, url_for, redirect
+from flask import Flask, url_for, redirect, request
+import json
 import logging
+import numpy
 
 # logging.basicConfig(filename='logs/all.log', format='%(asctime)s - %(message)s', level=logging.DEBUG)
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def index():
   return redirect(url_for('static', filename='index.html'))
+
+@app.route('/solution')
+def solution():
+  linalg = json.loads(request.args.get('linalg'))
+  A = numpy.array(linalg['A'])
+  b = numpy.array(linalg['b'])
+  solution = numpy.linalg.solve(A, b).tolist()
+  return json.dumps(solution)
 
 if __name__ == "__main__":
   app.run()
---
+++ static/js/linalg.js
@@ -0,0 +1,92 @@
+$(function() {
+  function solve_1d(factors) {
+    var A = [[factors[0]]]
+    var b = [[factors[1]]]
+    $.get('/solution?linalg=' + JSON.stringify({"A": A, "b": b}))
+     .done(function(data) {
+        var $solution = $('.solution')
+        var solution = JSON.parse(data)
+        var vars = ['x', 'y', 'z']
+        $solution.empty()
+        for (var i = 0; i < solution.length; i++) {
+          $solution.append('<p><var>' + vars[i] + '</var> = ' + solution[i][0] + '</p>')
+        }
+        $solution.show()
+        $solution.removeClass('animated rubberBand').addClass('animated rubberBand').one(
+          'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+            $(this).removeClass('animated rubberBand')
+          }
+        )
+      })
+     .fail(function() {
+        var $solution = $('.solution')
+        $solution.empty()
+        $solution.append('<span class="glyphicon glyphicon-remove-circle" aria-hidden="true"></span>&nbsp;这个题太难了我不会')
+        $solution.show()
+        $solution.removeClass('animated rubberBand').addClass('animated rubberBand').one(
+          'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+            $(this).removeClass('animated rubberBand')
+          }
+        )
+      })
+  }
+
+  function solve_if_all_factors_are_given() {
+    var factors = []
+    var all_factors_are_given = true
+    $('.factor > input').each(function() {
+      var val = $(this).val()
+      if (/^[\+\-]?[0-9]+$/g.test(val)) {
+        factors.push(parseInt(val, 10))
+      } else {
+        all_factors_are_given = false
+      }
+    })
+    if (all_factors_are_given) {
+      if (factors.length === 2) {
+        solve_1d(factors)
+      } else if (factors.length === 6) {
+        console.log('solving 2d')
+      } else if (factors.length === 12) {
+        console.log('solving 3d')
+      }
+    }
+  }
+
+  $('[data-toggle="tooltip"]').tooltip()
+
+  $('.factor').click(function() {
+    var $this = $(this)
+    var $editing = $('.editing')
+    if ($editing.length === 0) {
+      $this.addClass('editing')
+      $this.children('input').select()
+    } else if ($(event.target).parents('.editing').length === 0) {
+      $editing.removeClass('animated shake').addClass('animated shake').one(
+        'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+          $(this).removeClass('animated shake')
+        }
+      )
+    }
+  })
+
+  $('.factor').focusout(function(event) {
+    var $this = $(this)
+    var val = $this.children('input').val()
+    if (/^[\+\-]?[0-9]+$/g.test(val)) {
+      $this.removeClass('editing')
+      solve_if_all_factors_are_given()
+    } else {
+      $this.removeClass('animated shake').addClass('animated shake').one(
+        'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+          $(this).removeClass('animated shake')
+        }
+      )
+    }
+  })
+
+  $('.factor > input').keyup(function() {
+    var $this = $(this)
+    $this.prev().text($this.val())
+  })
+})
